bec92df07900fdf94123261b61389ba47e10e55f
雷全 <leiquan@live.com>
2015-07-09 14:37:12 +0800

    format dateDiff
    format dateDiff

--- LeiquanLibrary.js
+++ LeiquanLibrary.js
@@ -3,22 +3,18 @@
 //You Should Use These Method After Create A New Object By These Classes!
 
 //计算天数差的函数，通用  
-			    	function  DateDiff(sDate1,  sDate2){    //sDate1和sDate2是2002-12-18格式  
-			    			
-			    			var  aDate,  oDate1,  oDate2,  iDays;
-			    			
-			    			aDate  =  sDate1.split("-");
-			    			oDate1  =  new  Date(aDate[0]  ,  aDate[1]  ,  aDate[2]);//转换为12-18-2002格式  
-			    			
-			    			
-			    			aDate  =  sDate2.split("-")  
-			    			oDate2  =  new  Date(aDate[0] ,    aDate[1]  ,    aDate[2]);
-			    			
-			    			
-			    			iDays  =  parseInt(Math.abs(oDate1  -  oDate2)  /  1000  /  60  /  60  /24);//把相差的毫秒数转换为天数  
-			    			return  iDays+1;
-			    			
-			    			}
+function  DateDiff(sDate1,  sDate2){    //sDate1和sDate2是2002-12-18格式
+
+	var  aDate,  oDate1,  oDate2,  iDays;
+
+	aDate  =  sDate1.split("-");
+	oDate1  =  new  Date(aDate[0]  ,  aDate[1]  ,  aDate[2]);//转换为12-18-2002格式
+	aDate  =  sDate2.split("-");
+	oDate2  =  new  Date(aDate[0] ,    aDate[1]  ,    aDate[2]);
+
+	iDays  =  parseInt(Math.abs(oDate1  -  oDate2)  /  1000  /  60  /  60  /24);//把相差的毫秒数转换为天数
+	return  iDays+1;
+}
 
 //ClassSwitchTool
 var ClassSwitchTool = function(oSwitchBox, aImgUrls) {
